82385022a3df8ebf025a9409c4ee47bf0a7d5110
KALU56 <kalkidanbehailu55@gmail.com>
2025-03-25 20:48:47 +0300

    header logo

--- e-self/app/(components)/LandingHeader.tsx
+++ e-self/app/(components)/LandingHeader.tsx
@@ -10,9 +10,10 @@ export default function LandingHeader() {
     <header className="fixed top-0 left-0 w-full bg-white shadow-md p-4 flex justify-between items-center z-50">
       <div className="flex items-center">
         {/* Logo or Brand Name */}
-        <Link href="/" className="text-2xl font-bold text-[#8E1616]">
-          E-Learn Platform
-        </Link>
+        <Link href="/dashboard" className="text-xl font-bold min-w-fit">
+        <span className="text-[#1D1616]">E</span>
+        <span className="text-[#8E1616]">-Self</span>
+      </Link>
       </div>
 
       {/* Profile Section */}
