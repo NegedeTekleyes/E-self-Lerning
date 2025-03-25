7e5c6207125c5f20e454561fb3f9ae542a05ca20
KALU56 <kalkidanbehailu55@gmail.com>
2025-03-25 22:03:08 +0300

    payment

--- e-self/app/(student)/payment/half/page.tsx
+++ e-self/app/(student)/payment/half/page.tsx
@@ -2,14 +2,14 @@
 
 import { useRouter } from "next/navigation";
 
-export default function FullPaymentPage() {
+export default function HalfPaymentPage() {
   const router = useRouter();
 
   return (
     <div className="container mx-auto p-6 max-w-lg text-center">
-      <h1 className="text-3xl font-bold mb-6">full Payment</h1>
+      <h1 className="text-3xl font-bold mb-6">Half Payment</h1>
       <p className="text-lg text-gray-700 mb-4">
-        You are about to make a **full payment** for your selected courses.
+        You are about to make a **half payment** for your selected courses.
       </p>
       <button
         className="bg-[#8E1616] text-white px-5 py-2 rounded hover:bg-[#D84040] transition-colors focus:outline-none focus:ring-2 focus:ring-[#D84040] w-full"
