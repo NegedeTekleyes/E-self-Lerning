a5313d8dad96c952c9a50b45d675b880e148e594
KALU56 <kalkidanbehailu55@gmail.com>
2025-03-25 20:00:29 +0300

    landing header wideth

--- e-self/app/(components)/LandingHeader.tsx
+++ e-self/app/(components)/LandingHeader.tsx
@@ -3,20 +3,16 @@ import Link from 'next/link';
 
 export default function LandingHeader() {
   return (
-    <header className="flex items-center justify-between p-4 bg-[#EEEEEE] shadow-md w-full">
+    <header className="w-screen flex items-center justify-between p-4 bg-[#EEEEEE] shadow-md shadow-gray-500">
       <Link href="/" className="text-xl font-bold">
         <span className="text-[#1D1616]">E</span>
         <span className="text-[#8E1616]">-Self</span>
       </Link>
 
       <nav className="flex items-center gap-6">
-    
         <Link href="/instructor/signup" className="hover:text-[#8E1616]">
           Instructor
         </Link>
-        {/* <Link href="/signin" className="bg-[#8E1616] px-4 py-2 rounded hover:bg-[#D84040] text-white">
-          Sign In
-        </Link> */}
       </nav>
     </header>
   );
