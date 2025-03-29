0f3f24ac2ec36170f9faebc563f1be535d02634e
KALU56 <kalkidanbehailu55@gmail.com>
2025-03-29 21:07:10 +0300

    feat(hero):Update hero section image

--- e-self/app/(components)/student/Hero.tsx
+++ e-self/app/(components)/student/Hero.tsx
@@ -24,9 +24,9 @@ const Hero = () => {
   }, []);
 
   return (
-    <div className="relative bg-white overflow-hidden z-40"> {/* Changed bg-[#EEEEEE] to bg-white */}
+    <div className="relative bg-white overflow-hidden z-40">
       <div className="max-w-7xl mx-auto">
-        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32"> {/* Changed bg-[#EEEEEE] to bg-white */}
+        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
           <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
@@ -60,11 +60,11 @@ const Hero = () => {
       </div>
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <Image
-          src={isSmallScreen ? "/hero-.png" : "/Adobe Express - file.png"}
-          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+          src={isSmallScreen ? "/Adobe Express - file (1).png" : "/Adobe Express - file.png"}
+          className="w-full object-contain"  // Changed object-cover to object-contain
           alt="Hero Image"
-          width={1000}  // Adjust the width as needed
-          height={600} // Adjust the height as needed
+          width={600} 
+          height={500} // Adjust the height as needed
         />
       </div>
     </div>
