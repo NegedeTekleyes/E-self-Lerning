038c0346eab2af2e8ebd1bb9ccb7abdd26e7c74f
KALU56 <kalkidanbehailu55@gmail.com>
2025-03-29 20:48:08 +0300

    feat(hero):Update hero section image

--- e-self/app/(components)/student/Hero.tsx
+++ e-self/app/(components)/student/Hero.tsx
@@ -1,7 +1,28 @@
-// app/(components)/Hero.tsx
-import Image from 'next/image';
+"use client";
 
-export default function Hero() {
+import Image from "next/image";
+import { useState, useEffect } from "react";
+
+const Hero = () => {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setIsSmallScreen(true);
+      } else {
+        setIsSmallScreen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Initial check
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="relative bg-white overflow-hidden z-40"> {/* Changed bg-[#EEEEEE] to bg-white */}
       <div className="max-w-7xl mx-auto">
@@ -39,15 +60,15 @@ export default function Hero() {
       </div>
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <Image
+          src={isSmallScreen ? "/hero-.png" : "/Adobe Express - file.png"}
           className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src="/Adobe Express - file.png"
-          alt=""
-          width={500}
-          height={500}
-          layout="responsive"
+          alt="Hero Image"
+          width={1000}  // Adjust the width as needed
+          height={600} // Adjust the height as needed
         />
-    
       </div>
     </div>
   );
-}
+};
+
+export default Hero;
