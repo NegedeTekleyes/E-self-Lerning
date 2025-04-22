645f7b1e7d337d6fde2222b180fbc1ab1645f6e9
KALU56 <kalkidanbehailu55@gmail.com>
2025-04-22 10:50:04 +0300

    course page

--- e-self/app/(components)/instructor/SummaryChart.tsx
+++
@@ -1,7 +0,0 @@
-export default function SummaryChart() {
-  return (
-    <div className="h-48 bg-gradient-to-r from-blue-100 to-green-100 rounded-lg flex items-center justify-center">
-      <span className="text-gray-500">[Insert Chart.js or Recharts Here]</span>
-    </div>
-  );
-}
